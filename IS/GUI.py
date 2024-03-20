418fe037768aa7e4cabc9ff731614fb1243ff3ea
Artem Yakhontov <72457086+Setet@users.noreply.github.com>
2024-03-20 17:00:17 +0300

    v1.01.01

--- IS/GUI.py
+++ IS/GUI.py
@@ -3,6 +3,7 @@ import time
 import tkinter as tk
 from tkinter import *
 from tkinter import scrolledtext, messagebox
+from tkinter.ttk import Style
 
 import networkx as nx
 import matplotlib.pyplot as plt
@@ -17,25 +18,42 @@ def draw_colored_graph(G, pos, color_dict):
     nx.draw(G, pos, with_labels=True, node_color=node_colors, edge_color='black', width=2, alpha=0.8)
 
 
+# Функция осн интерфейса
 def gui():
+    # Инициализация окна
     window = Tk()
 
+    # Даю название окну
+    window.title("Раскраска графа")
+
+    # Узнаю размер экрана
     width = window.winfo_screenwidth()
     height = window.winfo_screenheight()
 
-    window.geometry("%dx%d" % (width, height))
-    window.title("Раскраска графа")
+    # Выставляю фулл размер
+    window.geometry(f"{width}x{height}+0+0")
 
+    style = Style()
+    style.theme_use("default")
+    style.configure("Label", font=("Helvetica", 14))
+    style.map("Label")
+
+    # Рисую начальное пустое поле под граф
     fig = plt.figure(figsize=(14, 14))
     canvas = FigureCanvasTkAgg(fig, master=window)
     canvas.draw()
     canvas.get_tk_widget().pack(side=tkinter.LEFT, fill=tkinter.BOTH)
 
+    # Функция для работы программы
     def start():
+        # Очищаю форму от предыдущих рисунков
         fig.clf()
-        # Данные для графа из нового словаря
-        graph_dict = generating_graph(txt_4_window.get(), txt_5_window.get())
-        color_graph = coloring_account(graph_dict, txt_1_window.get(), txt_2_window.get(), txt_3_window.get())
+
+        # Данные для графа
+        # Сам граф
+        graph_dict = generating_graph(input_field_4.get(), input_field_5.get())
+        # Цвета графа
+        color_graph = coloring_account(graph_dict, input_field_1.get(), input_field_2.get(), input_field_3.get())
 
         # Создаем граф и добавляем узлы и связи
         G = nx.Graph()
@@ -46,17 +64,21 @@ def gui():
 
         pos = nx.random_layout(G, seed=42)
 
-        # Отображение графа на tkinter canvas(нераскраш)
-        nx.draw(G, pos, with_labels=True, node_color='skyblue', edge_color='black', width=2, alpha=0.8)
+        # Отображение графа на tkinter canvas(не раскрашенный)
+        nx.draw(G, pos, with_labels=True, edge_color='black', width=2, alpha=0.8)
+
+        # Рисую то что получилось
         canvas.draw()
 
+        # Обновляю окно
         window.update()
 
         # Задержка в сек
         time.sleep(10)
 
         fig.clf()
-        # Отображение графа на tkinter canvas(рас)
+
+        # Отображение графа на tkinter canvas(раскрашенный)
         draw_colored_graph(G, pos, color_graph)
         canvas.draw()
 
@@ -64,50 +86,66 @@ def gui():
         messagebox.showinfo('Уведомление', 'Готово')
 
     def delete_lab():
-        txt_window.delete(1.0, END)
+        scrolled_text.delete(1.0, END)
 
-    main_f_window = LabelFrame(window, text="Параметры")
-    left_f_window = Frame(main_f_window)
-    right_f_window = Frame(main_f_window)
-    txt_f_window = LabelFrame(window, text="Консоль лог")
+    main_frame = LabelFrame(window, text="Параметры", font=('Arial', 20))
 
-    lbl_1_window = Label(left_f_window, text="Размер популяции")
-    lbl_2_window = Label(left_f_window, text="Кол-во итераций")
-    lbl_3_window = Label(left_f_window, text="Коэффициент мутации")
-    lbl_4_window = Label(left_f_window, text="Кол-во вершин")
-    lbl_5_window = Label(left_f_window, text="Кол-во рёбер")
+    left_frame = Frame(main_frame)
+    right_frame = Frame(main_frame)
+    console_frame = LabelFrame(window, text="Консоль лог", font=('Arial', 20))
 
-    txt_1_window = Entry(right_f_window)
-    txt_2_window = Entry(right_f_window)
-    txt_3_window = Entry(right_f_window)
-    txt_4_window = Entry(right_f_window)
-    txt_5_window = Entry(right_f_window)
+    label_1 = Label(left_frame, text="Размер популяции", font=('Arial', 20))
 
-    txt_window = scrolledtext.ScrolledText(txt_f_window)
-    btn_del_window = Button(window, text="Очистить лог", command=delete_lab)
-    btn_window = Button(window, text="Выполнить", foreground="black", background="#00FFFF", command=start)
+    label_2 = Label(left_frame, text="Кол-во итераций", font=('Arial', 20))
 
-    main_f_window.pack(side=TOP, padx=5, pady=5, fill=BOTH, expand=True)
-    left_f_window.pack(side=LEFT, fill=BOTH, expand=True)
-    right_f_window.pack(side=RIGHT, fill=BOTH, expand=True)
+    label_3 = Label(left_frame, text="Коэффициент мутации", font=('Arial', 20))
 
-    lbl_1_window.pack(side=TOP, padx=5, pady=5, fill=BOTH)
-    lbl_2_window.pack(side=TOP, padx=5, pady=5, fill=BOTH)
-    lbl_3_window.pack(side=TOP, padx=5, pady=5, fill=BOTH)
-    lbl_4_window.pack(side=TOP, padx=5, pady=5, fill=BOTH)
-    lbl_5_window.pack(side=TOP, padx=5, pady=5, fill=BOTH)
+    label_4 = Label(left_frame, text="Кол-во вершин", font=('Arial', 20))
 
-    txt_1_window.pack(side=TOP, padx=5, pady=5, fill=BOTH)
-    txt_2_window.pack(side=TOP, padx=5, pady=5, fill=BOTH)
-    txt_3_window.pack(side=TOP, padx=5, pady=5, fill=BOTH)
-    txt_4_window.pack(side=TOP, padx=5, pady=5, fill=BOTH)
-    txt_5_window.pack(side=TOP, padx=5, pady=5, fill=BOTH)
+    label_5 = Label(left_frame, text="Кол-во рёбер", font=('Arial', 20))
 
-    txt_window.pack(padx=5, pady=5, fill=BOTH, expand=True)
+    input_field_1 = Entry(right_frame, font=('Arial', 20))
+    input_field_1.insert(0, "50")
 
-    btn_window.pack(side=BOTTOM, padx=5, pady=5, fill=BOTH, expand=True)
-    txt_f_window.pack(side=BOTTOM, padx=5, pady=5, fill=BOTH, expand=True)
-    btn_del_window.pack(side=BOTTOM, padx=5, pady=5, fill=BOTH, expand=True)
+    input_field_2 = Entry(right_frame, font=('Arial', 20))
+    input_field_2.insert(0, "100")
+
+    input_field_3 = Entry(right_frame, font=('Arial', 20))
+    input_field_3.insert(0, "0.1")
+
+    input_field_4 = Entry(right_frame, font=('Arial', 20))
+    input_field_4.insert(0, "10")
+
+    input_field_5 = Entry(right_frame, font=('Arial', 20))
+    input_field_5.insert(0, "5")
+
+    scrolled_text = scrolledtext.ScrolledText(console_frame)
+    delete_button = Button(window, text="Очистить лог", command=delete_lab, font=('Arial', 20))
+
+    run_button = Button(window, text="Выполнить", foreground="black", background="#00FFFF", command=start,
+                        font=('Arial', 20))
+
+    main_frame.pack(side=TOP, padx=5, pady=5, fill=BOTH, expand=True)
+    left_frame.pack(side=LEFT, fill=BOTH, expand=True)
+    right_frame.pack(side=RIGHT, fill=BOTH, expand=True)
+
+    label_1.pack(side=TOP, padx=5, pady=5, fill=BOTH)
+    label_2.pack(side=TOP, padx=5, pady=5, fill=BOTH)
+    label_3.pack(side=TOP, padx=5, pady=5, fill=BOTH)
+    label_4.pack(side=TOP, padx=5, pady=5, fill=BOTH)
+    label_5.pack(side=TOP, padx=5, pady=5, fill=BOTH)
+
+    input_field_1.pack(side=TOP, padx=5, pady=5, fill=BOTH)
+    input_field_2.pack(side=TOP, padx=5, pady=5, fill=BOTH)
+    input_field_3.pack(side=TOP, padx=5, pady=5, fill=BOTH)
+    input_field_4.pack(side=TOP, padx=5, pady=5, fill=BOTH)
+    input_field_5.pack(side=TOP, padx=5, pady=5, fill=BOTH)
+
+    scrolled_text.pack(padx=5, pady=5, fill=BOTH, expand=True)
+
+    run_button.pack(side=BOTTOM, padx=5, pady=5, fill=BOTH, expand=True)
+    console_frame.pack(side=BOTTOM, padx=5, pady=5, fill=BOTH, expand=True)
+    delete_button.pack(side=BOTTOM, padx=5, pady=5, fill=BOTH, expand=True)
 
     # Запускаем главный цикл tkinter
     tk.mainloop()
